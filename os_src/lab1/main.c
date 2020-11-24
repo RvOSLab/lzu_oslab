ec1cb1f17f996ef766d6f42c40993b3f7315d4a3
Kong Jun <kongjun18@outlook.com>
2020-11-24 11:29:14 +0800

    Correct the use of sbi_probe_extention()

--- os_src/lab1/main.c
+++ os_src/lab1/main.c
@@ -7,53 +7,46 @@ main()
 {
     struct sbiret ret;
     ret = sbi_probe_extension(TIMER_EXTENTION);
-    if (ret.error == SBI_SUCCESS)
+    if (ret.value != 0)
         kputs("TIMER_EXTENTION: available");
     else 
         kputs("TIMER_EXTENTION: unavailable");
 
     ret = sbi_probe_extension(SHUT_DOWN_EXTENTION);
-    if (ret.error == SBI_SUCCESS)
+    if (ret.value != 0)
         kputs("SHUT_DOWN_EXTENTION: available");
     else 
         kputs("SHUT_DOWN_EXTENTION: unavailable");
 
     ret = sbi_probe_extension(HART_STATE_EXTENTION);
-    if (ret.error == SBI_SUCCESS)
+    if (ret.value != 0)
         kputs("HART_STATE_EXTENTION: available");
     else 
         kputs("HART_STATE_EXTENTION: unavailable");
 
     ret = sbi_get_impl_id();
-    if (ret.error != SBI_SUCCESS)
+    switch (ret.value)
     {
-        kputs("sbi_get_impl_id() failed");
-    }
-    else
-    {
-        switch (ret.value)
-        {
-            case BERKELY_BOOT_LOADER:
-                kputs("Implemention ID: Berkely boot loader");
-                break;
-            case OPENSBI:
-                kputs("Implemention ID: OpenSBI");
-                break;
-            case XVISOR:
-                kputs("Implemention ID: XVISOR");
-                break;
-            case KVM:
-                kputs("Implemention ID: KVM");
-                break;
-            case RUSTSBI:
-                kputs("Implemention ID: RustSBI");
-                break;
-            case DIOSIX:
-                kputs("Implemention ID: DIOSIX");
-                break;
-            default:
-                kputs("Implemention ID: Unkonwn");
-        }
+        case BERKELY_BOOT_LOADER:
+            kputs("Implemention ID: Berkely boot loader");
+            break;
+        case OPENSBI:
+            kputs("Implemention ID: OpenSBI");
+            break;
+        case XVISOR:
+            kputs("Implemention ID: XVISOR");
+            break;
+        case KVM:
+            kputs("Implemention ID: KVM");
+            break;
+        case RUSTSBI:
+            kputs("Implemention ID: RustSBI");
+            break;
+        case DIOSIX:
+            kputs("Implemention ID: DIOSIX");
+            break;
+        default:
+            kputs("Implemention ID: Unkonwn");
     }
     kputs("Hello LZU OS");
     while (1)

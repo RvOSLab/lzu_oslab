ca6debef974b47f0143576f4791d34483ee225b5
Kong Jun <kongjun18@outlook.com>
2020-12-07 11:58:34 +0800

    Basically complete physical memory management

--- os/include/stddef.h
+++ os/include/stddef.h
@@ -7,5 +7,6 @@ typedef unsigned int uint32_t;
 typedef signed long long int64_t;
 typedef unsigned long long uint64_t;
 typedef unsigned long long size_t;
-
+typedef signed long long ssize_t;
+typedef uint64_t intptr_t;
 #endif /* end of include guard: STDDEF_H */
--- os/lib/sbi.c
+++ os/lib/sbi.c
@@ -118,3 +118,62 @@ sbi_shutdown()
         : "memory"
         );
 }
+
+void
+kputs(const char *msg)
+{
+    for ( ; *msg != '\0'; ++msg )
+    {
+        sbi_console_putchar(*msg);
+    }
+    sbi_console_putchar('\n');
+}
+
+void
+print_system_infomation()
+{
+    struct sbiret ret;
+    ret = sbi_probe_extension(TIMER_EXTENTION);
+    if (ret.value != 0)
+        kputs("TIMER_EXTENTION: available");
+    else 
+        kputs("TIMER_EXTENTION: unavailable");
+
+    ret = sbi_probe_extension(SHUT_DOWN_EXTENTION);
+    if (ret.value != 0)
+        kputs("SHUT_DOWN_EXTENTION: available");
+    else 
+        kputs("SHUT_DOWN_EXTENTION: unavailable");
+
+    ret = sbi_probe_extension(HART_STATE_EXTENTION);
+    if (ret.value != 0)
+        kputs("HART_STATE_EXTENTION: available");
+    else 
+        kputs("HART_STATE_EXTENTION: unavailable");
+
+    ret = sbi_get_impl_id();
+    switch (ret.value)
+    {
+        case BERKELY_BOOT_LOADER:
+            kputs("Implemention ID: Berkely boot loader");
+            break;
+        case OPENSBI:
+            kputs("Implemention ID: OpenSBI");
+            break;
+        case XVISOR:
+            kputs("Implemention ID: XVISOR");
+            break;
+        case KVM:
+            kputs("Implemention ID: KVM");
+            break;
+        case RUSTSBI:
+            kputs("Implemention ID: RustSBI");
+            break;
+        case DIOSIX:
+            kputs("Implemention ID: DIOSIX");
+            break;
+        default:
+            kputs("Implemention ID: Unkonwn");
+    }
+}
+

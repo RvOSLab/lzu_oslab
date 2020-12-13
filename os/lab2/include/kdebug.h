33c6bcfc3f9c4481067563fe7db174d1156b38af
ytf4425 <93yutf@gmail.com>
2020-12-13 21:59:17 +0800

    add makefiles and move *.h

---
+++ os/lab2/include/kdebug.h
@@ -0,0 +1,12 @@
+#ifndef KDEBUG_H
+#define KDEBUG_H
+#include <stdarg.h>
+#include <stddef.h>
+#include <sbi.h>
+int kputs(const char *msg);
+// TODO
+// int kprintf(const char *fmt, ...);
+void kputchar(int ch);
+void do_panic(const char* file, int line, const char* fmt, ...);
+#endif /* end of include guard: KDEBUG_H */
+

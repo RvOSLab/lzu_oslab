b0c41d0a59e617fc6daaa7a76bae3c0ab06129a3
Kong Jun <kongjun18@outlook.com>
2020-12-18 21:09:30 +0800

    modify HIGH_MEM to 0x88000000

--- lab1/include/mm.h
+++ lab1/include/mm.h
@@ -4,11 +4,13 @@
 #define PAGE_SIZE       4096
 #define FLOOR(addr)     ((addr) / PAGE_SIZE * PAGE_SIZE)
 #define CEIL(addr)      (((addr) / PAGE_SIZE + ((addr) % PAGE_SIZE != 0)) * PAGE_SIZE)
+#define DEVICE_START    0x10000000
+#define DEVICE_END      0x10010000
 #define MEM_START       0x80000000
 #define MEM_END         0x88000000
 #define SIB_START       0x80000000
 #define SBI_END         0x82000000
-#define HIGH_MEM        FLOOR(0x87FF0000)
+#define HIGH_MEM        0x88000000
 #define LOW_MEM         CEIL(0x83000000)
 #define PAGING_MEMORY   (1024 * 1024 * 128)
 #define PAGING_PAGES    (PAGING_MEMORY>>12)

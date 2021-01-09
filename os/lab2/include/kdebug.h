f091ba230e534142d2ed93757457350348f28b7e
ytf4425 <93yutf@gmail.com>
2021-01-09 22:17:15 +0800

    fix a variable name bug

--- os/lab2/include/kdebug.h
+++ os/lab2/include/kdebug.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 #include <sbi.h>
 int kputs(const char *msg);
-uint64_t kprintf(const char* _Format, ...);
+uint64_t kprintf(const char* fmt, ...);
 void kputchar(int ch);
 void do_panic(const char* file, int line, const char* fmt, ...);
 #endif /* end of include guard: KDEBUG_H */
--- os/lab2/lib/kdebug.c
+++ os/lab2/lib/kdebug.c
@@ -38,7 +38,7 @@ void do_panic(const char* file, int line, const char* fmt, ...)
     kprintf("--------------------------------------------------------------------------\n");
 }
 
-uint64_t kprintf(const char* _Format, ...)
+uint64_t kprintf(const char* fmt, ...)
 {
     va_list ap;
 	uint64_t val;

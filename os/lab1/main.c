254a144889d050670af1787103d1b884d256b425
Kong Jun <kongjun18@outlook.com>
2021-01-20 16:51:43 +0800

    Finish the merge of lab1, interrupt, lab2 and lab4

--- os/lab1/main.c
+++ os/lab1/main.c
@@ -1,67 +1,71 @@
 #include <stddef.h>
 #include <sbi.h>
-static void kputs(const char *msg);
+void kputchar(int ch);
+int kputs(const char *msg);
 
-int 
-main()
+int main()
 {
-    struct sbiret ret;
-    ret = sbi_probe_extension(TIMER_EXTENTION);
-    if (ret.value != 0)
-        kputs("TIMER_EXTENTION: available");
-    else 
-        kputs("TIMER_EXTENTION: unavailable");
+	struct sbiret ret;
+	ret = sbi_probe_extension(TIMER_EXTENTION);
+	if (ret.value != 0)
+		kputs("TIMER_EXTENTION: available");
+	else
+		kputs("TIMER_EXTENTION: unavailable");
 
-    ret = sbi_probe_extension(SHUT_DOWN_EXTENTION);
-    if (ret.value != 0)
-        kputs("SHUT_DOWN_EXTENTION: available");
-    else 
-        kputs("SHUT_DOWN_EXTENTION: unavailable");
+	ret = sbi_probe_extension(SHUT_DOWN_EXTENTION);
+	if (ret.value != 0)
+		kputs("SHUT_DOWN_EXTENTION: available");
+	else
+		kputs("SHUT_DOWN_EXTENTION: unavailable");
 
-    ret = sbi_probe_extension(HART_STATE_EXTENTION);
-    if (ret.value != 0)
-        kputs("HART_STATE_EXTENTION: available");
-    else 
-        kputs("HART_STATE_EXTENTION: unavailable");
+	ret = sbi_probe_extension(HART_STATE_EXTENTION);
+	if (ret.value != 0)
+		kputs("HART_STATE_EXTENTION: available");
+	else
+		kputs("HART_STATE_EXTENTION: unavailable");
 
-    ret = sbi_get_impl_id();
-    switch (ret.value)
-    {
-        case BERKELY_BOOT_LOADER:
-            kputs("Implemention ID: Berkely boot loader");
-            break;
-        case OPENSBI:
-            kputs("Implemention ID: OpenSBI");
-            break;
-        case XVISOR:
-            kputs("Implemention ID: XVISOR");
-            break;
-        case KVM:
-            kputs("Implemention ID: KVM");
-            break;
-        case RUSTSBI:
-            kputs("Implemention ID: RustSBI");
-            break;
-        case DIOSIX:
-            kputs("Implemention ID: DIOSIX");
-            break;
-        default:
-            kputs("Implemention ID: Unkonwn");
-    }
+	ret = sbi_get_impl_id();
+	switch (ret.value) {
+	case BERKELY_BOOT_LOADER:
+		kputs("Implemention ID: Berkely boot loader");
+		break;
+	case OPENSBI:
+		kputs("Implemention ID: OpenSBI");
+		break;
+	case XVISOR:
+		kputs("Implemention ID: XVISOR");
+		break;
+	case KVM:
+		kputs("Implemention ID: KVM");
+		break;
+	case RUSTSBI:
+		kputs("Implemention ID: RustSBI");
+		break;
+	case DIOSIX:
+		kputs("Implemention ID: DIOSIX");
+		break;
+	default:
+		kputs("Implemention ID: Unkonwn");
+	}
+
     kputs("Hello LZU OS");
     while (1)
         ; /* infinite loop */
     return 0;
 }
 
-
-static void
-kputs(const char *msg)
+void kputchar(int ch)
 {
-    for ( ; *msg != '\0'; ++msg )
-    {
-        sbi_console_putchar(*msg);
+    sbi_console_putchar(ch);
+}
+
+int kputs(const char* msg)
+{
+    const char* ret = msg;
+    for (; *msg != '\0'; ++msg) {
+        kputchar(*msg);
     }
-    sbi_console_putchar('\n');
+    kputchar('\n');
+    return ret == msg;
 }
 

aed6bf9fe806c294b381e0f2b1268f9a3b5d6652
Kong Jun <kongjun18@outlook.com>
2021-02-11 17:20:43 +0800

    Use use sys_init() to init process 0

--- os/lab5/init/main.c
+++ os/lab5/init/main.c
@@ -6,9 +6,6 @@
 #include <clock.h>
 #include <syscall.h>
 
-/* 创建系统调用 testsyscall() */
-_syscall0(int, testsyscall)
-
 int main()
 {
     print_system_infomation();
@@ -16,13 +13,9 @@ int main()
     mem_test();
     trap_init();
     sched_init();
-    clock_init();
-    int i = 10000;
-    while (i--);
     kputs("Hello LZU OS");
-    /* 使用 ecall 实现系统调用，此时系统处于内核态，
-     * 系统认为 ecall 是 SBI 调用，因此无现象*/
-    testsyscall();
+    init_task0();
+    clock_init();
     while (1)
         ; /* infinite loop */
     return 0;
--- os/lab5/kernel/syscall.c
+++ os/lab5/kernel/syscall.c
@@ -7,20 +7,11 @@
 #include <syscall.h>
 
 extern int sys_fork(struct trapframe *);
-
-/**
- * @brief 测试系统调用
- * 本占据一个系统调用号，用于测试系统调用是否正常，将来会被删除
- */
-static int test_syscall(struct trapframe *tf)
-{
-    kprintf("test_syscall: successfull\n");
-    return 0;
-}
+extern int sys_init(struct trapframe *);
 
 /**
  * @brief 系统调用表
  * 存储所有系统调用的指针的数组，系统调用号是其中的下标。
  * 所有系统调用都通过系统调用表调用
  */
-fn_ptr syscall_table[] = {test_syscall, sys_fork};
+fn_ptr syscall_table[] = {sys_init, sys_fork};

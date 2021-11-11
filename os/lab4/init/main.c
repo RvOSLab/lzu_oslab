e84b8be20dc352f9b2ac0eaee80a50cb528b34ee
Hanabi <93yutf@gmail.com>
2021-11-11 14:43:34 +0800

    try to add malloc

--- os/lab4/init/main.c
+++ os/lab4/init/main.c
@@ -7,7 +7,11 @@ int main()
     print_system_infomation();
     mem_init();
     mem_test();
+    uint64_t * new = (uint64_t *)malloc(100);
+    kprintf("new: %p\n", new);
+    free_s(new,100);
     kputs("Hello LZU OS");
+
     while (1)
         ; /* infinite loop */
     return 0;

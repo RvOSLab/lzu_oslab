02c08189d5cd9a6c152a0b3ee835f915c52a266d
MaPl <mapl@york.moe>
2022-01-17 19:36:30 +0800

    [lab6] device/irq: add irq device

---
+++ os/lab6/include/device/irq.h
@@ -0,0 +1,22 @@
+#ifndef DEVICE_IRQ_H
+#define DEVICE_IRQ_H
+
+#include <stddef.h>
+#include <device.h>
+
+#define IRQ_INTERFACE_BIT (1 << 11)
+
+struct irq_descriptor {
+    const char* name;
+    void (*handler)();
+};
+
+/* 初始状态: 不使能任何IRQ, 阈值为最低 */
+struct irq_device {
+    void (*enable_irq)(struct device *dev, uint32_t hart_id, uint32_t irq_id);
+    void (*set_threshold)(struct device *dev, uint32_t hart_id, uint32_t threshold);
+    void (*set_priority)(struct device *dev, uint32_t irq_id, uint32_t priority);
+    void (*set_handler)(struct device *dev, uint32_t hart_id, uint32_t irq_id, struct irq_descriptor* descriptor);
+};
+
+#endif

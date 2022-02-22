811019076bd8bfe32460abab95040fa201909656
MaPl <mapl@york.moe>
2022-02-22 14:46:48 +0800

    [lab6] drivers/virtio: add virtio_device_probe()

--- os/lab6/drivers/loader.c
+++ os/lab6/drivers/loader.c
@@ -5,10 +5,12 @@
 #include <device/reset/sifive_test.h>
 #include <device/irq/plic.h>
 #include <device/serial/uart8250.h>
+#include <device/virtio.h>
 
 struct device_driver *driver_list[] = {
     &test_driver,
     &plic_driver,
     &uart8250_driver,
+    &virtio_driver,
     NULL
 };
---
+++ os/lab6/include/device/virtio.h
@@ -0,0 +1,9 @@
+#ifndef DEVICE_VIRTIO_H
+#define DEVICE_VIRTIO_H
+
+#include <stddef.h>
+#include <device.h>
+
+extern struct device_driver virtio_driver;
+
+#endif

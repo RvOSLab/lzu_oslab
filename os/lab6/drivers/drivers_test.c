a6a58ea09ddfa313d836f0a142c96412daa5843f
MaPl <mapl@york.moe>
2022-02-22 17:42:07 +0800

    [lab6] drivers: add char_dev_test()

--- os/lab6/drivers/drivers_test.c
+++ os/lab6/drivers/drivers_test.c
@@ -1,9 +1,22 @@
 #include <device.h>
-#include <device/reset.h>
 #include <device/reset/sifive_test.h>
+#include <device/serial/uart8250.h>
+#include <kdebug.h>
 
 void drivers_test() {
     struct device *dev = get_dev_by_major_minor(SIFIVE_TEST_MAJOR, 1);
     struct reset_device * test = dev->get_interface(dev, RESET_INTERFACE_BIT);
     // test->shutdown(dev);
+}
+
+void char_dev_test() {
+    struct device *dev = get_dev_by_major_minor(UART8250_MAJOR, 1);
+    struct serial_device *test = dev->get_interface(dev, SERIAL_INTERFACE_BIT);
+    const char *test_str = "mapl char dev test\n";
+    test->request(dev, test_str, strlen(test_str), 0);
+
+    char buffer[256];
+    test->request(dev, buffer, 4, 1);
+    buffer[4] = '\0';
+    kputs(buffer);
 }
--- os/lab6/include/device.h
+++ os/lab6/include/device.h
@@ -54,6 +54,7 @@ uint32_t device_table_get_next_minor(uint32_t major, uint32_t minor_start);
 struct device *get_dev_by_major_minor(uint32_t major, uint32_t minor);
 
 void drivers_test();
+void char_dev_test();
 
 void mem_resource_map(struct driver_resource *res);
 
